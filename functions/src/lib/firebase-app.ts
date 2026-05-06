e21895aec36cd75e8a042e4095c2d27ffdcd7f7e
fnobi <fnobi0.8@gmail.com>
2026-05-06 13:42:27 +0900

    config参照なしに

--- functions/src/lib/firebase-app.ts
+++ functions/src/lib/firebase-app.ts
@@ -2,13 +2,12 @@ import { type App, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
-import { config as functionsConfig } from "firebase-functions";
 
 let currentApp: App | null = null;
 
 const getApp = () => {
   if (!currentApp) {
-    currentApp = initializeApp(functionsConfig().firebase);
+    currentApp = initializeApp();
   }
   return currentApp;
 };
